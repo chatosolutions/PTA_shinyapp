d0606422dc9b00af4d9b48ebea793dda65f41d94
Chato Solutions <66096410+chatosolutions@users.noreply.github.com>
2021-01-20 14:14:20 -0600

    Add files via upload

--- server.R
+++ server.R
@@ -11,12 +11,14 @@ library(shiny)
 library(plotly)
 library(dplyr)
 library(emdbook)
+library(minpack.lm)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
     
     v <- reactiveValues(datatest = NULL, 
                         datamodel = NULL,
+                        datamodelDD_BU = NULL,
                         xint1 = 1, 
                         yint1 = 0, 
                         xint2.1 = 1, yint2.1 = 0, xint2.2 = 1, yint2.2 = 0)
@@ -26,8 +28,18 @@ shinyServer(function(input, output, session) {
         
         pwf.data <- read.csv(file=data_origin()$datapath) #read.csv("E:/SHINY_APP/pruebas/wt.csv")
         colnames(pwf.data) <- c("t", "pwf")
-        pwf.data$dp <- pwf.data$pwf[1] - pwf.data$pwf
-        pwf.data$Ddp <- -derivative.Bourdet(log(pwf.data$t),pwf.data$pwf)
+        
+        if(input$type_WT == "Drowdown"){
+          pwf.data$dp <- pwf.data$pwf[1] - pwf.data$pwf
+          pwf.data$Ddp <- -derivative.Bourdet(log(pwf.data$t),pwf.data$pwf)
+        }
+        
+        if(input$type_WT == "Build-up"){
+          pwf.data$dp <- pwf.data$pwf - pwf.data$pwf[1]
+          time_eq <- (input$tp*pwf.data$t)/(input$tp + pwf.data$t)
+          pwf.data$Ddp <- derivative.Bourdet(log(time_eq),pwf.data$pwf)
+        }
+        
         last_Dpd <-  dplyr::last(pwf.data$Ddp)
         v$yint1 <- last_Dpd
         y_escala <- lseq(0.000001,1000000, 13)
@@ -86,16 +98,36 @@ shinyServer(function(input, output, session) {
     
     #Boton Load de ejemplos
     observeEvent(input$loadex,{
-      file_example <- data.frame(name = "Example 1", datapath = "https://raw.githubusercontent.com/chatosolutions/PTA_shinyapp/main/data/Example_4.3_AWTI.csv", stringsAsFactors = FALSE)
-      data_origin(file_example)
       
-      updateNumericInput(session,"qo","Oil rate (STB/D)", value = "125")
-      updateNumericInput(session,"poro","Porosity (fraction)", value = "0.22")
-      updateNumericInput(session,"h", "Thickness (ft)", value = "32")
-      updateNumericInput(session,"rw", "Well redius (ft)", value = "0.25")
-      updateNumericInput(session,"bo","Oil Formation Volume (Bo, bbl/STB)", value = "1.152")
-      updateNumericInput(session,"vis", "Oil Viscosity (cp)", value = "2.122")
-      updateNumericInput(session,"ct", "Total compressibility (psi^-1)", value = "0.0000109")
+      v$datamodel <- NULL
+      v$datamodelDD_BU <- NULL
+      if(input$type_WT == "Drowdown"){ 
+          file_example <- data.frame(name = "Example 1", datapath = "https://raw.githubusercontent.com/chatosolutions/PTA_shinyapp/main/data/Example_4.3_AWTI.csv", stringsAsFactors = FALSE)
+          data_origin(file_example)
+          
+          updateNumericInput(session,"qo","Oil rate (STB/D)", value = "125")
+          updateNumericInput(session,"poro","Porosity (fraction)", value = "0.22")
+          updateNumericInput(session,"h", "Thickness (ft)", value = "32")
+          updateNumericInput(session,"rw", "Well redius (ft)", value = "0.25")
+          updateNumericInput(session,"bo","Oil Formation Volume (Bo, bbl/STB)", value = "1.152")
+          updateNumericInput(session,"vis", "Oil Viscosity (cp)", value = "2.122")
+          updateNumericInput(session,"ct", "Total compressibility (psi^-1)", value = "0.0000109")
+        }
+      
+      if(input$type_WT == "Build-up"){ 
+        file_example <- data.frame(name = "Example 1", datapath = "https://raw.githubusercontent.com/chatosolutions/PTA_shinyapp/main/data/BU_Ex_7.6.csv", stringsAsFactors = FALSE)
+        data_origin(file_example)
+        
+        updateNumericInput(session,"qo","Oil rate (STB/D)", value = "150")
+        updateNumericInput(session,"poro","Porosity (fraction)", value = "0.2")
+        updateNumericInput(session,"h", "Thickness (ft)", value = "50")
+        updateNumericInput(session,"rw", "Well redius (ft)", value = "0.33")
+        updateNumericInput(session,"bo","Oil Formation Volume (Bo, bbl/STB)", value = "1.5")
+        updateNumericInput(session,"vis", "Oil Viscosity (cp)", value = "0.5")
+        updateNumericInput(session,"ct", "Total compressibility (psi^-1)", value = "0.00005")
+        updateNumericInput(session,"tp", "Production time (hour)", value = "1000")
+      }
+
       #v$yint1 <- 25.6
       #v$xint2.1 <- 0.00086
       #v$yint2.1 <- 1
@@ -210,29 +242,275 @@ shinyServer(function(input, output, session) {
         #   logt <- lseq(ti, dplyr::last(pwf.data$t),500)
         # }
         
-        logt <- pwf.data$t
-        td <- (0.0002637*k*(logt))/(poro*vis*ct*rw^2)
-        cD <- (0.8936*C)/(h*poro*ct*rw^2)
-        pwD <- Stehfest_inversion(td,cD,s)
-        pwf <- pi - (pwD*141.2*Bo*vis*q)/(h*k)
-
-        if(pwf.data$t[1] == 0){
-          pwf[1] = pi
+        if(input$type_WT == "Drowdown"){
+          logt <- pwf.data$t
+          pwD <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,k,logt,C,s)
+          pwf <- pi - (pwD*141.2*Bo*vis*q)/(h*k)
+          
+          if(pwf.data$t[1] == 0){
+            pwf[1] = pi
+          }
+          
+          dp <- pwf[1] - pwf
+          Ddp <- -derivative.Bourdet(log(logt),pwf)
+          
+          v$datamodel <- data.frame(t = logt, pwf = pwf, dp = dp, Ddp = Ddp) 
         }
         
-        dp <- pwf[1] - pwf
-        Ddp <- -derivative.Bourdet(log(logt),pwf)
-        #print(pwf)
+        if(input$type_WT == "Build-up"){
+          tp <- input$tp
+          dt <- pwf.data$t #(input$tp*pwf.data$t)/(input$tp + pwf.data$t)
+          te <- (input$tp*pwf.data$t)/(input$tp + pwf.data$t)
+          tp.dt <- pwf.data$t + input$tp
+          pwf <-  pwf.data$pwf[1]
+          
+          pwD.dt <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,k,dt,C,s)
+          pwD.dpdt <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,k,tp.dt,C,s)
+          pwD.BU <- pwD.dpdt[1] + pwD.dt - pwD.dpdt
+          pwD.BU[1] <- 0
+          pws <- pwf + (pwD.BU*141.2*Bo*vis*q)/(h*k)
+          
+          dp <- pws - pws[1]
+          Ddp <- derivative.Bourdet(log(te),pws)
+          
+          v$datamodel <- data.frame(t = pwf.data$t, pwf = pws, dp = dp, Ddp = Ddp)
+          
+          #Drowdown section in BU
+          DD_t <- c(0.0000001,lseq(0.001, input$tp,nrow(pwf.data)))
+          DD_t_sort <- sort(DD_t, decreasing = TRUE)
+          pwD.DDdp <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,k,DD_t_sort,C,s)
+          pwf_DD <- pwf + (pwD.DDdp*141.2*Bo*vis*q)/(h*k)
+          
+          v$datamodelDD_BU <- data.frame(t = DD_t, pwf = pwf_DD)
+        }
         
-        v$datamodel <- data.frame(t = logt, pwf = pwf, dp = dp, Ddp = Ddp) 
-        head(v$datamodel)
+        
+        head(v$datamodel) 
+      }
+      
+    })
+    
+    #Boton fit model
+    observeEvent(input$fit_model,{
+      
+      qp <- input$qo
+      porop <- input$poro
+      hp <- input$h
+      rwp <- input$rw
+      Bop <- input$bo
+      pip <- input$pi
+      visp <- input$vis
+      ctp <- input$ct
+      kp <- input$k
+      sp <- input$s
+      Cp <- input$C
+      
+      if(!is.null(v$datatest)){
+        pwf.data <- v$datatest
+        
+        if(input$type_WT == "Drowdown"){
+          logt <- pwf.data$t
+          
+          residFun <- function(parS,observed,logt,pi = pip ,poro = porop,ct = ctp,rw = rwp,Bo = Bop,vis = visp,q = qp,h = hp){
+            
+            pwD <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,parS$k,logt,parS$C,parS$s)
+            pwf_cal <- pip - (pwD*141.2*Bo*vis*q)/(h*parS$k)
+            
+            if(logt[1] == 0){
+              pwf_cal[1] = pip
+            }
+            
+            observed - pwf_cal
+          }
+          
+          parStart <- list(k = kp, C = Cp, s = sp)
+          fit_model <- nls.lm(par = parStart, fn = residFun, observed =  pwf.data$pwf,
+                              logt =  logt, control = nls.lm.control(nprint=1))
+          
+          fit_coeff <- as.list(coef(fit_model))
+          pwD <- pwD.calDD(porop,ctp,rwp,Bop,visp,qp,hp,fit_coeff$k,logt,fit_coeff$C,fit_coeff$s)
+          pwf_cal <- pip - (pwD*141.2*Bop*visp*qp)/(hp*fit_coeff$k)
+          
+          if(pwf.data$t[1] == 0){
+            pwf_cal[1] = pip
+          }
+          
+          dp <- pwf_cal[1] - pwf_cal
+          Ddp <- -derivative.Bourdet(log(logt),pwf_cal)
+          
+          v$datamodel <- data.frame(t = logt, pwf = pwf_cal, dp = dp, Ddp = Ddp) 
+          
+          }
+
+        if(input$type_WT == "Build-up"){
+          
+          tp <- input$tp
+          dtp <- pwf.data$t #(input$tp*pwf.data$t)/(input$tp + pwf.data$t)
+          te <- (input$tp*pwf.data$t)/(input$tp + pwf.data$t)
+          tp.dtp <- pwf.data$t + input$tp
+          pwfp <-  pwf.data$pwf[1]
+          
+          residFun <- function(parS,observed,dt = dtp,tp.dt = tp.dtp,pwf = pwfp,poro = porop,ct = ctp,rw = rwp,Bo = Bop,vis = visp,q = qp,h = hp){
+            
+            pwD.dt <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,parS$k,dt,parS$C,parS$s)
+            pwD.dpdt <- pwD.calDD(poro,ct,rw,Bo,vis,q,h,parS$k,tp.dt,parS$C,parS$s)
+            pwD.BU <- pwD.dpdt[1] + pwD.dt - pwD.dpdt
+            pwD.BU[1] <- 0
+            pws_cal <- pwf + (pwD.BU*141.2*Bo*vis*q)/(h*parS$k)
+            
+            (observed - pws_cal)/observed
+          }
+          
+          parStart <- list(k = kp, C = Cp, s = sp)
+          fit_model <- nls.lm(par = parStart, fn = residFun, observed =  pwf.data$pwf,
+                              dt =  dtp, control = nls.lm.control(nprint=1))
+          
+          
+          #calculate with fit coef
+          fit_coeff <- as.list(coef(fit_model))
+          pwD.dt <- pwD.calDD(porop,ctp,rwp,Bop,visp,qp,hp,fit_coeff$k,dtp,fit_coeff$C,fit_coeff$s)
+          pwD.dpdt <- pwD.calDD(porop,ctp,rwp,Bop,visp,qp,hp,fit_coeff$k,tp.dtp,fit_coeff$C,fit_coeff$s)
+          pwD.BU <- pwD.dpdt[1] + pwD.dt - pwD.dpdt
+          pwD.BU[1] <- 0
+          pws_cal <- pwfp + (pwD.BU*141.2*Bop*visp*qp)/(hp*fit_coeff$k)
+
+          dp <- pws_cal - pws_cal[1]
+          Ddp <- derivative.Bourdet(log(te),pws_cal)
+          
+          v$datamodel <- data.frame(t = pwf.data$t, pwf = pws_cal, dp = dp, Ddp = Ddp)
+          
+          #Drowdown section in BU
+          DD_t <- c(0.0000001,lseq(0.001, input$tp,nrow(pwf.data)))
+          DD_t_sort <- sort(DD_t, decreasing = TRUE)
+          pwD.DDdp <- pwD.calDD(porop,ctp,rwp,Bop,visp,qp,hp,fit_coeff$k,DD_t_sort,fit_coeff$C,fit_coeff$s)
+          pwf_DD <- pwfp + (pwD.DDdp*141.2*Bop*visp*qp)/(hp*fit_coeff$k)
+          
+          v$datamodelDD_BU <- data.frame(t = DD_t, pwf = pwf_DD)
+          
+        }
+        
+        updateNumericInput(session, "k","Permeability (md)", value = fit_coeff$k)
+        updateNumericInput(session, "s","Skin", value = fit_coeff$s)
+        updateNumericInput(session, "C","WBS coefficient (bbl/psi)", value = fit_coeff$C)
+        
+        }
+    })
+    
+    
+    #Plot production data
+    output$plot_rate <- renderPlotly({ 
+      
+      if(!is.null(v$datatest)){
+        
+        pwf.data <- v$datatest
+        
+        if(input$type_WT == "Drowdown"){
+          time <- c(0,pwf.data$t)
+          q <- c(0, rep(input$qo,length(pwf.data$t)))
+          
+          plot_pre <- plot_ly() %>%
+            add_lines(x = time, y = q, line = list(color = "green", width = 5), name = ~"rate") %>%
+            layout(
+              xaxis = list(title = "time, hr"),
+              yaxis = list(title = "rate, STB")
+            )
+        }
+        
+        if(input$type_WT == "Build-up"){
+          
+          time <- c(0,0,input$tp,input$tp, input$tp+max(pwf.data$t))
+          q <- c(0,input$qo,input$qo,0,0)
+          
+          plot_pre <- plot_ly() %>%
+            add_lines(x = time, y = q, line = list(color = "green", width = 5), name = ~"rate") %>%
+            layout(
+              xaxis = list(title = "time, hr"),
+              yaxis = list(title = "rate, STB")
+            )
+        }
+
+        plot_pre
+      }else{
+        p <- ggplot() +
+          xlab("time, hr") +
+          ylab("rate, STB") +
+          scale_x_continuous(limits = c(0, 100)) +
+          scale_y_continuous(limits = c(0, 100)) 
+        p <- ggplotly(p)
+      }
+      
+      })
+    
+    #Plot pressure data
+    output$plot_pressure <- renderPlotly({ 
+      
+      if(!is.null(v$datatest)){
+       
+        pwf.data <- v$datatest
+        
+        if(input$type_WT == "Drowdown"){ 
+          plot_pre <- plot_ly() %>%
+            add_markers(x = pwf.data$t, y = pwf.data$pwf,marker = list(color = "green"), name = ~"pressure") %>%
+            layout(
+              xaxis = list(title = "time, hr"),
+              yaxis = list(title = "pressure, psia")
+            )
+          
+          if(!is.null(v$datamodel)){
+            datamodel <- v$datamodel
+            time <- datamodel$t
+            plot_pre <- plot_pre %>%
+              add_lines(x = time, y = datamodel$pwf, line = list(color = "blue")  , name = ~"Model pressure")
+          }
+        }
+        
+        if(input$type_WT == "Build-up"){
+          
+          time <- input$tp + pwf.data$t
+          plot_pre <- plot_ly() %>%
+            add_markers(x = time, y = pwf.data$pwf,marker = list(color = "green"), name = ~"pressure") %>%
+            layout(
+              xaxis = list(title = "time, hr", range = c(0,input$tp+max(pwf.data$t))),
+              yaxis = list(title = "pressure, psia")
+            )
+          
+          
+          if(!is.null(v$datamodel)){
+            datamodel <- v$datamodel
+            datamodel.pwfDD <-v$datamodelDD_BU
+            time <- input$tp + datamodel$t
+            plot_pre <- plot_pre %>%
+              add_lines(x = time, y = datamodel$pwf, line = list(color = "blue")  , name = ~"Model pressure") %>%
+              add_lines(x = datamodel.pwfDD$t, y = datamodel.pwfDD$pwf, line = list(color = "blue")  , name = ~"Model pressure")
+
+          }
+          
+        }
+        
+        plot_pre
+      }else{
+        p <- ggplot() +
+          xlab("time, hr") +
+          ylab("pressure, psia") +
+          scale_x_continuous(limits = c(0, 100)) +
+          scale_y_continuous(limits = c(0, 100)) 
+        p <- ggplotly(p)
       }
       
     })
 
 })
 
-
+#Drawdown
+pwD.calDD <- function(poro,ct,rw,Bo,vis,q,h,k,logt,C,s){
+  
+  td <- (0.0002637*k*(logt))/(poro*vis*ct*rw^2)
+  cD <- (0.8936*C)/(h*poro*ct*rw^2)
+  pwD <- Stehfest_inversion(td,cD,s)
+  #pwf <- pi - (pwD*141.2*Bo*vis*q)/(h*k)
+  
+  return(pwD)
+}
 
 
 finite.differences.FW <- function(x, y) {
@@ -328,5 +606,7 @@ Stehfest_inversion <- function(t, cD, s){
   return(PwD)
   
 }
+
+
 
 
--- ui.R
+++ ui.R
@@ -21,25 +21,41 @@ shinyUI(fluidPage(
         sidebarPanel(
             tags$a(href = "https://www.chatosolutions.com/", "Home", 
                    target="_blank"),
-          selectInput("type_WT","Type", c("Drowdown")),
+          selectInput("type_WT","Type", c("Drowdown", "Build-up")),
           h3("Input data"),
           
           fileInput('filePTA', label=NULL, multiple = FALSE, accept = NULL, width =NULL, buttonLabel = "Browse...", placeholder = "No file selected"),
           
+          #conditionalPanel(
+              #condition = "input.type_WT == 'Drowdown'",
+              column(
+                  6,
+                  numericInput("qo", "Oil rate (STB/D)", value = ""),
+                  numericInput("poro", "Porosity (fraction)", value = ""),
+                  numericInput("h", "Thickness (ft)", value = ""),
+                  numericInput("rw", "Well redius (ft)", value = "")
+              ),
+              
+              column(
+                  6,
+                  conditionalPanel(
+                     condition = "input.type_WT == 'Drowdown'", 
+                     numericInput("pi", "Initial pressure (psi)", value = "")
+                     ),
+                  
+                    conditionalPanel(
+                    condition = "input.type_WT == 'Build-up'", 
+                    numericInput("tp", "Production time (hour)", value = "")
+                    )
+                  ,
+                  numericInput("bo", "Oil Formation Volume (Bo, bbl/STB)", value = ""),
+                  numericInput("vis", "Oil Viscosity (cp)", value = ""),
+                  numericInput("ct", "Total compressibility (psi^-1)", value = "")
+              ),
+          #), 
           
-          column(6,
-                 numericInput("qo","Oil rate (STB/D)", value = ""),
-                 numericInput("poro","Porosity (fraction)", value = ""),
-                 numericInput("h", "Thickness (ft)", value = ""),
-                 numericInput("rw", "Well redius (ft)", value = "")
-          ),
           
-          column(6,
-                 numericInput("pi","Initial pressure (psi)", value = ""),
-                 numericInput("bo","Oil Formation Volume (Bo, bbl/STB)", value = ""),
-                 numericInput("vis", "Oil Viscosity (cp)", value = ""),
-                 numericInput("ct", "Total compressibility (psi^-1)", value = "")
-          ),
+
           
           actionButton("loadex", "Load Example"),
           
@@ -49,7 +65,8 @@ shinyUI(fluidPage(
           numericInput("k","Permeability (md)", value = ""),
           numericInput("s","Skin", value = ""),
           numericInput("C","WBS coefficient (bbl/psi)", value = ""),
-          actionButton("gen_model", "Generate model")
+          actionButton("gen_model", "Generate model"),
+          actionButton("fit_model", "Fit model")
           
 
           
@@ -57,21 +74,45 @@ shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotlyOutput("p", height = "70vh"),
             
-            column(4,
-                   numericInput("tdpr","t\u0394p'r", value = ""),
-                   numericInput("dtr","\u0394tr", value = "")
-            ),
-            
-            column(4,
-                   numericInput("dpr","\u0394pr", value = ""),
-                   numericInput("dtw","\u0394tw", value = "")
-            ),
-            
-            column(4,
-                   numericInput("dpw","\u0394pw", value = "")
+            tabsetPanel(id = "tabs_PVT",
+                        
+                        tabPanel(title="Production data",
+                                 
+                                 plotlyOutput("plot_rate", height = "30vh"),
+                                 plotlyOutput("plot_pressure", height = "30vh")
+                                 
+                        ),
+                        
+                        tabPanel(title="log-log analysis",
+                                 
+                                 plotlyOutput("p", height = "70vh"),
+                                 
+                                 column(4,
+                                        numericInput("tdpr","t\u0394p'r", value = ""),
+                                        numericInput("dtr","\u0394tr", value = "")
+                                 ),
+                                 
+                                 column(4,
+                                        numericInput("dpr","\u0394pr", value = ""),
+                                        numericInput("dtw","\u0394tw", value = "")
+                                 ),
+                                 
+                                 column(4,
+                                        numericInput("dpw","\u0394pw", value = "")
+                                 )
+                                 
+                        )#,
+                        
+                        #tabPanel(title="Semi-log analysis"
+                                      
+                        #)
+
+                        
             )
+            
+            
+
             
             
             
